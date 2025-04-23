b45774620033691a31892ab51aceb0d66d092d12
ngn <ngn@ngn.tf>
2025-04-23 17:44:06 +0300

    update the test script to provide more output
    Signed-off-by: ngn <ngn@ngn.tf>

--- tests/test-history.sh
+++ tests/test-history.sh
@@ -2,12 +2,17 @@
 
 file="tests/content/b/README.md"
 commits="$(git log -20 --pretty=format:%h -- "${file}")"
+body=$(curl -s "${url}/_/history/${file}")
 history="$(
-  curl -s "${url}/_/history/${file}" | \
+  echo "${body}" | \
   htmlq --text '.content table td:first-child'
 )"
 
 [[ "${commits}" == "${history}" ]] && ok
 
-echo "${commits} doesn't match with ${history}"
+echo "${commits} does not match with ${history}"
+echo "--- body ---"
+echo "${body}"
+echo "------------"
+
 fail
