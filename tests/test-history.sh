2089c5269e19534200fe4aee3f77f48bff761e77
ngn <ngn@ngn.tf>
2025-04-23 18:02:24 +0300

    remove verbose output from the history test script
    Signed-off-by: ngn <ngn@ngn.tf>

--- tests/test-history.sh
+++ tests/test-history.sh
@@ -2,17 +2,10 @@
 
 file="tests/content/b/README.md"
 commits="$(git log -20 --pretty=format:%h -- "${file}")"
-body=$(curl -s "${url}/_/history/${file}")
 history="$(
-  echo "${body}" | \
+  curl -s "${url}/_/history/${file}" | \
   htmlq --text '.content table td:first-child'
 )"
 
 [[ "${commits}" == "${history}" ]] && ok
-
-echo "${commits} does not match with ${history}"
-echo "--- body ---"
-echo "${body}"
-echo "------------"
-
 fail
